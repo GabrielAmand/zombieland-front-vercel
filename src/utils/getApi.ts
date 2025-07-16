24fc0a74d63c4359a812bc11a49620cfd82fea09
GabrielAmand <gabrielamand@yahoo.com>
2025-07-16 14:20:45 +0200

    remove unused data folder

--- src/utils/getApi.ts
+++ src/utils/getApi.ts
@@ -1,9 +1,6 @@
 export function getApiUrl(): string {
-	if (typeof window === "undefined") {
-		// Exécuté côté serveur (SSR)
-		return process.env.API_URL ?? "http://back:5000/api";
-	} else {
-		// Exécuté côté client (CSR)
-		return process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5000/api";
+	if (!process.env.NEXT_PUBLIC_API_URL) {
+		throw new Error("NEXT_PUBLIC_API_URL n'est pas définie.");
 	}
+	return process.env.NEXT_PUBLIC_API_URL;
 }
