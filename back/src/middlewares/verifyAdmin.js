64e6420a15c6c620e5d3f5a0dbe3c9f61eeb22ad
GabrielAmand <gabrielamand@yahoo.com>
2025-07-17 09:37:06 +0200

    fetched the team project for updates

--- back/src/middlewares/verifyAdmin.js
+++
@@ -1,9 +0,0 @@
-export function verifyAdmin(req, res, next) {
-	if (!req.user) {
-		return res.status(401).json({ error: 'Utilisateur non authentifié' });
-	}
-	if (!req.user.admin) {
-		return res.status(403).json({ error: 'Accès refusé : rôle admin requis' });
-	}
-	next();
-}
--- back/src/routers/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import loginControllers from '../../controllers/loginControllers.js';
-
-const router = express.Router();
-
-// get one user
-router.post('/', loginControllers.getOneUser);
-
-export default router;
