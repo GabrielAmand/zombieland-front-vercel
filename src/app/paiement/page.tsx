7095b3a63895007bb38c78f180ca5434c83647ea
GabrielAmand <gabrielamand@yahoo.com>
2025-07-24 12:49:36 +0200

    pull from team project

--- src/app/paiement/page.tsx
+++ src/app/paiement/page.tsx
@@ -3,13 +3,14 @@ import { Suspense } from "react";
 
 
 export const metadata = {
-  title: "Paiement – Zombieland",
-  description: "Validez votre paiement pour Zombieland et confirmez votre réservation.",
+	title: "Paiement – Zombieland",
+	description:
+		"Validez votre paiement pour Zombieland et confirmez votre réservation.",
 };
 
 export default function PaiementPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start px-4 py-12 bg-background text-text">
+    <div className="px-4 py-6 max-w-6xl mx-auto">
       <Suspense  fallback={<p className="text-center mt-6">Chargement...</p>}>
         <PaiementSection />
       </Suspense>
